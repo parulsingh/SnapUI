a407f5ff4d4bba6ee90ad48472f4169dcafc6017
Parul Singh <parulsingh@berkeley.edu>
2014-06-25 15:29:54 -0700

    kendo stuff

--- SnapUI.Web/App_Start/BundleConfig.cs
+++ SnapUI.Web/App_Start/BundleConfig.cs
@@ -30,6 +30,16 @@ namespace SnapUI.Web
                 .Include("~/Content/toastr.css")
                 .Include("~/Content/app.css")
               );
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+            "~/Scripts/kendo/kendo.all.min.js",
+                // "~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+            "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+            "~/Content/kendo/kendo.common.min.css",
+            "~/Content/kendo/kendo.default.min.css"));
+
+
         }
 
         public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
