41898ebd4bae9ad73a495902b390d8de7a585d58
srhee93 <srhee93@gmail.com>
2014-07-24 09:10:26 -0700

    word-wrap; fixed BugCheck link for SCCM

---
+++ SnapUI.Common.Models/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapUI.Common.Models
+{
+    public class Manager
+    {
+        public string Name;
+        public List<string> Reports;
+        public Manager (string name, List<string> reports)
+        {
+            Name = name;
+            Reports = reports;
+        }
+    }
+}
