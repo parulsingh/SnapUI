7caeacdb67e46d98df04dc9538f27cf2e545de04
Parul Singh <parulsingh@berkeley.edu>
2014-07-25 11:07:38 -0700

    all queues and duration graphs added

--- SnapUI.Web/App/viewmodels/summary.js
+++ SnapUI.Web/App/viewmodels/summary.js
@@ -14,46 +14,111 @@
         jobsAndQueues: ko.observableArray([]),
         allQueues: ko.observableArray([]),
         compositionComplete: compositionComplete,
-        queueArray: ko.observableArray([])
+        queueArray: ko.observableArray([]),
+        durationDay: ko.observableArray([]),
+        durationWeek: ko.observableArray([]),
+        allQGraphData: ko.observableArray([]),
+        allQGraphDataDay: ko.observableArray([])
     };
     return vm;
 
     function compositionComplete() {
+        $('#allqueues').highcharts({
+            credits: {
+                enabled: false
+            },
+            chart: {
+                //renderTo: 'container',
+                type: 'column',
+                backgroundColor: 'transparent',
+                width: 800,
+                height: 450
+            },
+            title: {
+                text: "All Queues"
+            },
+            series: [{
+                showInLegend: true,
+                name: "Week",
+                color: 'red',
+                data: vm.allQGraphData()
+            }, {
+                showInLegend: true,
+                name: "24 hours",
+                color: 'blue',
+                data: vm.allQGraphDataDay()
+            }],
+            xAxis: {
+                categories: ["Completed", "Aborted", "In Progress", "Pending"]
+            },
+            yAxis: {
+                title: {
+                    text: '# of Jobs'
+                },
+            },
 
-        //$('#chart').highcharts({
-        //    chart: {
-        //        renderTo: 'container',
-        //        type: 'column',
-        //        backgroundColor: 'transparent',
-        //        width: 500
-        //    },
-        //    title: {
-        //        text: 'All Queues'
-        //    },
-        //    series: [{
-        //        name: "Week",
-        //        color: '#00B2EE',
-        //        data: vm.allQGraphData
-        //    }, {
-        //        name: "24 hours",
-        //        color: '#FF4C4C',
-        //        data: vm.allQGraphDataDay
-        //    }],
-        //    xAxis: {
-        //        categories: ["Aborted", "Completed", "In Progress", "Pending", "Cancelled", "woohoo"]
-        //    },
-        //    yAxis: {
-        //        title: {
-        //            text: 'Number of Jobs'
-        //        }
-        //    },
-        //    legend: {
-        //        itemStyle: {
-        //            fontWeight: 'none'
-        //        }
-        //    },
-        //}
-        //)
+            plotOptions: {
+                column: {
+                    borderWidth: '0',
+                    dataLabels: {
+                        enabled: true,
+                        color: 'black',
+                    },
+                    colorByPoint: true,
+                }
+            },
+            colors: ['#4AC948', '#FF6961', '#0198E1', '#878787']
+        }
+)
+        $('#duration').highcharts({
+            credits: {
+                enabled: false
+            },
+            chart: {
+                //renderTo: 'container',
+                type: 'column',
+                backgroundColor: 'transparent',
+                width: 800,
+                height: 450
+            },
+            title: {
+                text: "Time to Success" 
+            },
+            series: [{
+                showInLegend: true,
+                name: "Week",
+                color: 'red',
+                data: vm.durationWeek()
+            }, {
+                showInLegend: true,
+                name: "24 hours",
+                color: 'blue',
+                data: vm.durationDay()
+            }],
+            xAxis: {
+                categories: vm.allQueues()
+            },
+            yAxis: {
+                title: {
+                    text: 'Time to Completion (minutes)'
+                },
+            },
+
+            plotOptions: {
+                column: {
+                    borderWidth: '0',
+                    dataLabels: {
+                        enabled: true,
+                        color: 'black'
+                        
+                    },
+                    colorByPoint: true,
+                }
+            },
+            colors: ['#4AC948', '#FF6961', '#0198E1', '#878787']
+        }
+)
+
         for (var i = 0; i < vm.allQueues().length; i++) {
             $('#chart' + i).highcharts({
                 credits: {
@@ -70,15 +135,15 @@
                     text: vm.queueArray()[i][0]
                 },
                 series: [{
-                    showInLegend: false,
+                    showInLegend: true,
                     name: "Week",
                     color: 'red',
                     data: vm.queueArray()[i][1]
                 }, {
-                    showInLegend: false,
+                    showInLegend: true,
                     name: "24 hours",
                     color: 'blue',
-                    data: vm.queueArray()[i][2]                
+                    data: vm.queueArray()[i][2]
                 }],
                 xAxis: {
                     //categories: ["Aborted", "Completed", "In Progress", "Pending"]
@@ -89,11 +154,7 @@
                         text: '# of Jobs'
                     },
                 },
-                legend: {
-                    itemStyle: {
-                        fontWeight: 'none'
-                    }
-                },
+
                 plotOptions: {
                     column: {
                         borderWidth: '0',
@@ -125,51 +186,37 @@
                 self.allQueues = ko.observableArray(self.jobsAndQueues()[1]);
 
                 //objects for all queue info for a week
-                var allQueueData = {       
+                var allQueueData = {
                     Completed: 0,
                     Aborted: 0,
                     "In Progress": 0,
-                    Pending: 0,                    
+                    Pending: 0,
                     //numCheckins: self.allJobs().length
                 }
                 //objects for all queue info for a day
-                var allQueueDataDay = {           
+                var allQueueDataDay = {
                     Completed: 0,
                     Aborted: 0,
                     "In Progress": 0,
-                    Pending: 0,                    
+                    Pending: 0,
                     //numCheckins: 0
                 }
 
                 // array of objects holding info for specific queues for a week and day
-                var queueArray = []; 
+                var queueArray = [];
 
                 // populating the array with objects
-                //name week day
+                //name week day duration
                 for (var i = 0; i < self.allQueues().length; i++) {
-                    queueArray[i] = [self.allQueues()[i],[0,0,0,0,0],[0,0,0,0,0]];
-/*
-                        {
-                        name: self.allQueues()[i],
-                        Aborted: 0,
-                        Completed: 0,
-                        "In Progress": 0,
-                        Pending: 0,
-                        Cancelled: 0,
-                        numCheckins: 0,
-                        AbortedDay: 0,
-                        CompletedDay: 0,
-                        "In ProgressDay": 0,
-                        PendingDay: 0,
-                        CancelledDay: 0,
-                        numCheckinsDay: 0
-                    }
-                    */
+                    queueArray[i] = [self.allQueues()[i], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0]];
                 }
+
                 //var statusDict = { "Aborted": 0, "Completed": 1, "In Progress": 2, "Pending": 3, "Cancelled": 4, "numCheckins": 5 };
                 var statusDict = { "Completed": 0, "Aborted": 1, "In Progress": 2, "Pending": 3, };
                 var checkins = [];
                 var jobs = self.allJobs();
+                var numCompletedWeek = 0;
+                var numCompletedDay = 0;
                 // counting the aborted/completed.../.. values for specific queues for weeks and days
                 // and then incrementing corresponding object values
                 for (var i = 0; i < jobs.length; i++) {
@@ -191,37 +238,65 @@
                             //queueArray[j][1][5]++;
                             queueArray[j][1][4]++;
                             queueArray[j][1][statusDict[status]]++;
+                            
+                            console.log("status " + status + " jobid " + jobs[i].Jobid + " duration " + jobs[i].Duration);
+                            if (status == "Completed") {
+                                queueArray[j][3][0] += jobs[i].Duration;
+                                numCompletedWeek++;
+                            }
                             if (jobDate.getTime() == d.getTime()) {
                                 //queueArray[j][2][5]++;
                                 queueArray[j][2][4]++;
                                 queueArray[j][2][statusDict[status]]++;
-                                allQueueDataDay.numCheckins++;
+                                //allQueueDataDay.numCheckins++;
                                 allQueueDataDay[status]++;
+                                
+                                if (status == "Completed") {
+                                    queueArray[j][3][1] += jobs[i].Duration;
+                                    numCompletedDay++;
+                                }
                             }
                         }
                     }
                     allQueueData[status]++;
 
                 }
+
                 for (var j = 0; j < queueArray.length; j++) {
-                    checkins.push(queueArray[j][1].pop());
-                    checkins.push(queueArray[j][2].pop());
+                    var weekCheckins = checkins.push(queueArray[j][1].pop());
+                    var dayCheckins = checkins.push(queueArray[j][2].pop());
+                    queueArray[j][3][0] = queueArray[j][3][0] / numCompletedWeek;
+                    queueArray[j][3][1] = queueArray[j][3][1] / numCompletedDay;
                 }
+
+                //duration stuff
+                var durationWeek = [];
+                var durationDay = [];
+                for (var j = 0; j < queueArray.length; j++) {
+                    durationWeek.push(Math.round(queueArray[j][3][0]));
+                    durationDay.push(Math.round(queueArray[j][3][1]));
+                }
+              
 
                 // creating observables
                 self.queueArray = ko.observableArray(queueArray);
                 self.allQueueData = ko.observable(allQueueData);
                 self.allQueueDataDay = ko.observable(allQueueDataDay);
-                
+                self.durationWeek = ko.observable(durationWeek);
+                self.durationDay = ko.observable(durationDay);
+
                 // counting values to put in highcharts
-                self.allQGraphData = [];
+                allQGraphData = [];
                 for (elem in self.allQueueData()) {
-                    self.allQGraphData.push(self.allQueueData()[elem]);
+                    allQGraphData.push(self.allQueueData()[elem]);
                 }
-                self.allQGraphDataDay = [];
+                allQGraphDataDay = [];
                 for (elem in self.allQueueDataDay()) {
-                    self.allQGraphDataDay.push(self.allQueueDataDay()[elem]);
+                    allQGraphDataDay.push(self.allQueueDataDay()[elem]);
                 }
+                console.log("All Q grpah data " + allQGraphData);
+                self.allQGraphData = ko.observable(allQGraphData);
+                self.allQGraphDataDay = ko.observable(allQGraphDataDay);
             });
 
 
